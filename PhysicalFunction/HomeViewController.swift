81b31eb65e50c33765f41e80878734dd6508f0f8
uncsupport <uncsupport@ITSs-MacBook-Air.local>
2019-04-25 21:38:46 -0400

    Add database functionality

--- PhysicalFunction/HomeViewController.swift
+++ PhysicalFunction/HomeViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 import ResearchKit
+import Firebase
 
 class HomeViewController: UIViewController {
     //MARK: Properties
 
     var myString = String()
+
+    var docref: DocumentReference!
+    let userID = Auth.auth().currentUser?.uid
+    let email = Auth.auth().currentUser?.email
+    
+    
     @IBOutlet weak var PFScore: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var pfScore: Double = 0.0
+        let docref = Firestore.firestore().document("users/\(email ?? "0")")
+        
+        docref.getDocument{ (docSnapshot, error) in
+            guard let docSnapshot = docSnapshot, docSnapshot.exists else {return}
+            let myData = docSnapshot.data()
+            pfScore = myData!["PFScore"] as? Double ?? 0
+            print(pfScore)
+            self.PFScore.text = "\(pfScore)"
+        }
+        
         let tabbar = tabBarController as! DataController
-        PFScore.text = String(describing: tabbar.valuePFScore)
+        //PFScore.text = String(describing: tabbar.valuePFScore)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -30,7 +49,20 @@ class HomeViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         let tabbar = tabBarController as! DataController
-        PFScore.text = String(describing: tabbar.valuePFScore)
+        
+        var pfScore: Double = 0.0
+        
+        let docref = Firestore.firestore().document("users/\(userID ?? "0")")
+        docref.getDocument{ (docSnapshot, error) in
+            guard let docSnapshot = docSnapshot, docSnapshot.exists else {return}
+            let myData = docSnapshot.data()
+            pfScore = myData!["PFScore"] as? Double ?? 0
+            print(pfScore)
+            self.PFScore.text = "\(pfScore)"
+        }
+        
+        //PFScore.text = String(describing: tabbar.valuePFScore)
+        
     }
 
    
